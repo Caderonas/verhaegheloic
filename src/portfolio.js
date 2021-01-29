8f242b9f00c7af59b0a462974dbcd36b69c6a1bc
caderonas <loic1997@hotmail.com>
2021-01-29 13:15:22 +0100

    portfolio_bis

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,6 +11,23 @@ class portfolio extends React.Component {
                   <path d="M1909 109C1815.67 102.194 1638.14 82.9021 1549.28 69.1799C1444.77 53.0413 1342.3 28.3121 1234.02 17.8863C1139.62 8.79758 1049.2 6.75176 952.848 8.64405C743.669 12.7523 538.146 28.2254 331.101 40.6721C266.845 44.5349 201.916 48.3943 137.127 50.2631C120.601 50.7398 27.8496 60.6299 15 58.8659" />
                   <path d="M16 117C110.322 115.329 414.594 96.6478 505.652 85.6045C613.455 72.5306 721.832 56.9158 831.874 47.7882C898.327 42.2762 970.276 42.8026 1037.71 43.278C1406.67 45.8792 1619.86 114.419 1921 117" />
               </svg>
+              <ul>
+                  <li>
+                      <img alt="Blason de la ville de Mouscron" src="http://lamouscronnoise.be/Shared/images/logo_msc.svg"/>
+                      <h4>La Mouscronnoise</h4>
+                      <p>I made this website a few years ago for student group which I'm a part of for 4 years now.</p>
+                      <p>Most of work is on the backend in PHP.</p>
+                  </li>
+                  <li>
+                      <h4><a href="http://lamouscronnoise.be/Mouscron_Eurometropolitan_city/index.html" target="_blank" rel="noreferrer">Eurometropolitan</a></h4>
+                      <p>This website was asked by a friends who has to present his final study work as architect remotly.</p>
+                      <p>I made this easy way to discover his work with map and pictures.</p>
+                  </li>
+                  <li>
+                      <h4>And more to come...</h4>
+                  </li>
+              </ul>
+              <h3>Contacts</h3>
           </div>
       );
   }
